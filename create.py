af779d7373f11e23304f213e3225de968420502f
gracewa <33077294+gracewa@users.noreply.github.com>
2020-09-07 07:46:31 +0300

    Added code to save new user credentials to file

--- create.py
+++ create.py
@@ -3,25 +3,36 @@ from random import randint
 
 
 def new():
-    print('What would you like to do?' + '\n')
-    print('Enter 1 to create a new password manager account, 2 to view saved passwords:' + '\n')
-    selection = int(input())
-    if selection == 1:
-        print('Enter a username for your password manager account:' + '\n')
-        selected_username = str(input())
-        print('Enter a password for your password manager account:' + '\n')
-        selected_password = str(input())
-        print('Please enter your password again to confirm:' + '\n')
-        confirmed_password = str(input())
-        if selected_password != confirmed_password:
-            print('Your passwords did not match!')
-            print('Enter your password again:'+ '\n')
+    while True:
+        print('What would you like to do?' + '\n')
+        print('Enter 1 to create a new password manager account, 2 to log into your password manager account:' + '\n')
+        selection = int(input())
+        if selection == 1:
+            print('Enter a username for your password manager account:' + '\n')
+            selected_username = str(input())
+            print('Enter a password for your password manager account:' + '\n')
             selected_password = str(input())
-            print('Please enter your password one more time to confirm:' + '\n')
+            print('Please enter your password again to confirm:' + '\n')
             confirmed_password = str(input())
-            print('Congratulations ' + selected_username + '! Your password manager account has been created')
-        else:
-            print('Congratulations ' + selected_username + '! Your password manager account has been created')
+            if selected_password != confirmed_password:
+                print('Your passwords did not match!')
+                print('Enter your password again:'+ '\n')
+                selected_password = str(input())
+                print('Please enter your password one more time to confirm:' + '\n')
+                confirmed_password = str(input())
+                if selected_password == confirmed_password:
+                    print('Congratulations ' + selected_username + '! Your password manager account has been created')
+
+                else:
+                    print('Account not created because passwords failed to match again! Try again later')
+
+            else:
+                print('Congratulations ' + selected_username + '! Your password manager account has been created')
+        with open('users.txt', 'a') as userfile:
+            userfile.write(selected_username + '@@@' + selected_password + '\n')
+        continue
+        if selection == 2:
+
 
 
 
